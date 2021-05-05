387ce3899b6e2b0abdeaeba83b1dec0801229cf8
davlethanov <danildavlethanov@gmail.com>
2021-05-06 01:22:52 +0500

    Add Empty Api Controllers

---
+++ NBA.Web/Controllers/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBA.Web.Controllers
+{
+    [Route("api/players")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+    }
+}
